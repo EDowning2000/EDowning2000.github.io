a1366501296e8216bb5633da3734d917e1b86638
Emerson Downing <57017788+EDowning2000@users.noreply.github.com>
2020-01-23 13:52:22 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var questions = ["Can you add elements to a webpage straight from JavaScript?","What can be inside of a string in JavaScript?","What does a 'for loop' do in JavaScript?", "What is a good resource for JavaScript related questions?"]
+var firstQAnswers = ["Yes", "No", "There isnt a reason to", "This is a trick question"]
+var secondQAnswers = ["Single Words", "Phrases", "Numbers", "All of the above"]
+var thirdQAnswers = ["Its a function that moves over an array one index at a time","It puts the contents of an array in a big circle on the webpage", "Its not actually a thing", "It sends every photo on your phone directly to your grandmother"]
+var fourthQAnswers = ["W3 schools","Stackoverflow", "The book <i>Eloquent JavaScript</i> by Marijn Haverbeke","All of these are helpful"]
+
+
+$(".btn btn-primary").click(function(){
+  $(".intro").addClass("dontDisplay")
+})
