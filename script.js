4c2ad535dcb20d60db58463f359498464081065a
Emerson Downing <57017788+EDowning2000@users.noreply.github.com>
2020-01-27 18:31:36 -0500

    Add files via upload
    it all works finally

--- script.js
+++ script.js
@@ -1,18 +1,139 @@
-var questions = ["Can you add elements to a webpage straight from JavaScript?","What can be inside of a string in JavaScript?","What does a 'for loop' do in JavaScript?", "What is a good resource for JavaScript related questions?"]
+var questions = [
+  {
+    question: "Can you add elements to a webpage straight from JavaScript?",
+    answers: ["yes","no","why would you do this?"],
+    correctAnswer: 0
+  },
+  {
+    question: "What can be inside of a string in JavaScript?",
+    answers: ["numbers","letters","phrases","all of the above"],
+    correctAnswer: 3
+  },
+  {
+    question: "What does a 'for loop' do in JavaScript?",
+    answers: ["Loops through an array","Sends all of your photos to your grandmother"],
+    correctAnswer: 0
+  },
+  {
+    question: " What is a good resource for JavaScript related questions?",
+    answers: ["stackoverflow","reddit","w3schools","all of the above"],
+    correctAnswer: 3
+  }
+];
 
-var firstQAnswers = ["Yes", "No", "There isnt a reason to", "This is a trick question"]
-
-var secondQAnswers = ["Single Words", "Phrases", "Numbers", "All of the above"]
-
-var thirdQAnswers = ["Its a function that moves over an array one index at a time","It puts the contents of an array in a big circle on the webpage", "Its not actually a thing", "It sends every photo on your phone directly to your grandmother"]
-
-var fourthQAnswers = ["W3 schools","Stackoverflow", "The book <i>Eloquent JavaScript</i> by Marijn Haverbeke","All of these are helpful"]
-
-var startBtn = $('#startTimerBtn')
 
 
-document.getElementById("startTimerBtn").addEventListener("click", function(){
-  alert('hello world')
+
+
+var timerElement = document.querySelector("#timer")
+var displayedQElement = document.querySelector('#displayedQ')
+var choicesContainerElement =document.querySelector("#choicesContainer")
+var submitButton = document.querySelector("#submitBtn")
+var scoresContainer = document.querySelector(".scoresContainer")
+
+var currentTime;
+var timerInterval;
+var currentQuestionIndex =0; 
+
+function endQuiz (){
+  clearInterval(timerInterval);
+  currentQuestionIndex=0
+  $('.currentQ').hide()
+  $('.finalScreen').show()
+  $("#score").text(currentTime)
+
+}
+
+
+
+
+function startTimer() {
+  currentTime = 70;
+
+  timerElement.textContent=currentTime
+  timerInterval = setInterval(function() {
+    currentTime--;
+
+    if(currentTime===0){
+      endQuiz()
+    }
+
+
+    timerElement.textContent=currentTime; 
+    
+  }, 1000);
+}
+
+function validateAnswer(event){
+  var selectedAnswer = event.target.dataset.index;
+  var currentQuestion= questions[currentQuestionIndex];
+
+  if(currentQuestion.correctAnswer !== parseInt(selectedAnswer)){
+    currentTime -= 10
+  }
+
+  currentQuestionIndex++;
+
+  if (currentQuestionIndex<questions.length){
+    displayQuestion()
+  }
+  else{
+    endQuiz()
+  }
+}
+
+function displayQuestion(){
+  var currentQuestion= questions[currentQuestionIndex];
+
+  var answers = currentQuestion.answers
+  displayedQElement.textContent= currentQuestion.question;
+
+  choicesContainerElement.innerHTML= ""
+
+  for (var i=0; i<answers.length; i++){
+    var newButton = document.createElement("button")
+    newButton.textContent= answers[i]
+    newButton.setAttribute("data-index", i)
+    newButton.addEventListener("click", validateAnswer)
+
+   choicesContainerElement.append(newButton)
+  }
+}
+
+
+$(".startTimerBtn").on("click", function() {
+  $(".intro").addClass("dontDisplay");
+  $('.currentQ').show()
+  startTimer()
+  displayQuestion()
+});
+
+
+submitButton.addEventListener('click', function(event){
+ event.preventDefault ;
+  var inputBtn= document.getElementById("Your-initials").value
+ console.log(inputBtn)
+  
+ var finalScores = {
+   name : inputBtn , 
+   score : currentTime
+ }
+ console.log(finalScores)
+ localStorageInfo.push(finalScores)
+ console.log(localStorageInfo)
+ var pushableLocalStorage= JSON.stringify(localStorageInfo)
+ localStorage.setItem('topScores' ,pushableLocalStorage)
 })
 
+function scoresOnScreen(){
+  
+  var localStorageInfo = JSON.parse(localStorage.getItem("topScores"))
+console.log(localStorageInfo)
+if(localStorageInfo===null){
+  emptyScoresArray = '[]'
+  localStorage.setItem("topScores", emptyScoresArray)
+  // var localStorageInfo = JSON.parse(localStorage.getItem("topScores" ))
+}
+console.log(localStorageInfo)
 
+}
